bf658cde267f9b16375f9190706853ed0ec10f15
deerajreddyaalla <deerajgnt@gmail.com>
2020-11-27 23:47:51 -0600

    Few Changes to add more conditions for getting Weather Info

--- NomadFinal/app/src/main/java/com/example/nomadfinal/data/Data.kt
+++ NomadFinal/app/src/main/java/com/example/nomadfinal/data/Data.kt
@@ -2,7 +2,7 @@ package com.example.nomadfinal.data
 
 data class Data(
     val location: String,
-    val temperature: Int,
+    val temperature: Int?,
     val weather: String,
     val icon: String,
     val time: String,
