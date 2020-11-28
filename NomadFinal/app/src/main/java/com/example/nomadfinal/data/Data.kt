e669999773b913f57e61c0da46fd6bd01e247e74
deerajreddyaalla <deerajgnt@gmail.com>
2020-11-27 21:09:15 -0600

    Added more conditions to get the weather based on time

---
+++ NomadFinal/app/src/main/java/com/example/nomadfinal/data/Data.kt
@@ -0,0 +1,10 @@
+package com.example.nomadfinal.data
+
+data class Data(
+    val location: String,
+    val temperature: Int,
+    val weather: String,
+    val icon: String,
+    val time: String,
+    val timeZone: String
+)
